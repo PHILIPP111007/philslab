2378cfdcc8eee4c48832ef5d4e44d18cb4ca0a59
Philipp Roshchin <FRoschin@biotc.ru>
2026-08-29 14:52:11 +0300

    AppBottomBlock

--- Frontend/react/app/src/App.jsx
+++ Frontend/react/app/src/App.jsx
@@ -62,6 +62,7 @@ export default function App() {
                                     <Route path="*" element={<ErrorPage />} />
                                 </Routes>
                             </Suspense>
+                            <div className="AppBottomBlock"></div>
                         </div>
                     </ThemeProvider>
                 </BrowserRouter>
